c11b5b336c2b5c5f8b618f065edf43566c11664b
Abishek Krishnan <abishek.krishnan@outlook.com>
2025-12-28 12:58:08 -0800

    Refactor factorial function and update tests to remove skips

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,22 @@
-const factorial = function() {
+const factorial = function (n) {
+  if (!isNumber(n) || n < 0) {
+    return undefined;
+  }
 
+  if (n === 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
 };
+
+function isNumber(n) {
+  if (typeof n === "number" && Number.isInteger(n)) {
+    return true;
+  }
+
+  return false;
+}
 
 // Do not edit below this line
 module.exports = factorial;
