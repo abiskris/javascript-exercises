a76c87a1e1300ad96eef2c5a36077231cf1d18b6
Abishek Krishnan <abishek.krishnan@outlook.com>
2025-12-28 16:34:56 -0800

    Refactor contains function and enable previously skipped tests

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,21 @@
-const contains = function() {
-  
+const contains = function (obj, val) {
+  // Handle NaN
+  if (typeof obj !== "object" || obj === null) {
+    return obj === val || (Number.isNaN(obj) && Number.isNaN(val));
+  }
+
+  // Direct reference match for objects/arrays
+  if (obj === val) return true;
+
+  // Recurse through object values
+  for (const key in obj) {
+    if (contains(obj[key], val)) {
+      return true;
+    }
+  }
+
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
