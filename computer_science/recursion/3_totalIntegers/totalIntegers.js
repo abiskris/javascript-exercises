47c8c6615923a48a105a8338a57fe47a5a19f167
Abishek Krishnan <abishek.krishnan@outlook.com>
2025-12-28 17:24:50 -0800

    Refactor totalIntegers function and enable previously skipped tests

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,32 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (input) {
+  if (typeof input !== "object") {
+    return undefined;
+  }
+
+  return totalIntegersHelper(input, 0);
 };
-  
+
+function totalIntegersHelper(input, numberOfIntegers) {
+  if (typeof input === "number") {
+    return 1;
+  } else {
+    for (let key in input) {
+      if (typeof input[key] === "number" && Number.isInteger(input[key])) {
+        numberOfIntegers++;
+      } else if (typeof input[key] === "object" && input[key] !== null) {
+        numberOfIntegers += totalIntegersHelper(input[key], 0);
+      }
+    }
+  }
+  return numberOfIntegers;
+}
+
 // Do not edit below this line
 module.exports = totalIntegers;
+
+/*
+```javascript
+totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // returns 7
+totalIntegers({ a: 1, b: { a: [5, 10], b: 11 } }); // returns 4
+```
+*/
